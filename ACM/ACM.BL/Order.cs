99190baa66641c6d3172344a3949728cb70e82d7
edwardgreenwood38 <edwardgreenwood38@gmail.com>
2019-09-02 12:22:06 -0400

    end methods module 5

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public int OrderId { get; set; }
+        public DateTimeOffset? OrderDate { get; set; }
+
+
+        /// <summary>
+        /// Validates Product
+        /// </summary>
+        /// <return></return>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Retrieve one order.
+        /// </summary>
+        /// <returns></returns>
+        public Customer Retrieve(int orderId)
+        {
+            // code that retrieves the defined customer
+
+            return new Customer();
+        }
+
+
+        /// <summary>
+        /// Retieve all orders.
+        /// </summary>
+        public List<Customer> Retrieve()
+        {
+            // code that retrieves all of the customers
+
+            return new List<Customer>();
+        }
+
+
+        /// <summary>
+        /// Saves the current order.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // code that saves the difined customer
+
+            return true;
+        }
+    }
+}
