e748bae4f51b233dee5a5d0f6b01c19ed3cb12f5
edwardgreenwood38 <edwardgreenwood38@gmail.com>
2019-09-08 15:04:16 -0400

    components and interfaces Module 9 and 10

---
+++ ACM/Tests/Acme.CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Acme.CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acme.CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8e34c25-40a0-432d-9766-cf26e9a46af9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ACM/Tests/Acme.CommonTest/StringHandlerTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Acme.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class StringHandlerTest
+    {
+        [TestMethod]
+        public void InsertSpacesTestValid()
+        {
+            // Arrange
+            var source = "SonicScrewdriver";
+            var expected = "Sonic Screwdriver";
+            //var handler = new StringHandler();
+
+            // Act
+            var actual = source.InsertSpaces();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void InsertSpacesTestWithExistingSpace()
+        {
+            // Arrange
+            var source = "Sonic Screwdriver";
+            var expected = "Sonic Screwdriver";
+
+            // Act
+            var actual = StringHandler.InsertSpaces(source);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void InsertSpacesTestEmptySource()
+        {
+            // Arrange
+            string source = "";
+            string expected = "";
+
+            // Act
+            var actual = StringHandler.InsertSpaces(source);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void InsertSpacesTestNullSource()
+        {
+            // Arrange
+            string source = null;
+            string expected = "";
+
+            // Act
+            var actual = StringHandler.InsertSpaces(source);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
